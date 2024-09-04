3a21a077959574316acd8c83d585083c3d0fbff8
Enol Álvarez <enol826@gmail.com>
2024-09-04 15:45:41 +0200

    WIP

---
+++ vara-extrinsics/logging.go
@@ -0,0 +1,7 @@
+package soltransactions
+
+import (
+	"github.com/streamingfast/logging"
+)
+
+var zlog, tracer = logging.PackageLogger("sol-transactions", "github.com/streamingfast/substreams-codegen/codegen/sol-transactions")
