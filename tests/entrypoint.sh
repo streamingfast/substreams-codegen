2f6a30a313e3f5c269dc16cb87895d762db2d5e7
arnaudberger <arnaudberger84@gmail.com>
2024-10-18 15:29:50 -0400

    fix not local path

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -2,15 +2,15 @@
 set -euxo pipefail
 
 listen_address=
-if [[ $TEST_LOCAL_CODEGEN == "true" ]]; then
-      if [[ $GITHUB_ACTIONS == "true" ]]; then
+if [[ ${TEST_LOCAL_CODEGEN:-false} == "true" ]]; then
+      if [[ ${GITHUB_ACTIONS:-false} == "true" ]]; then
          # Codegen address when running test
          listen_address="http://172.17.0.1:9000"
       else
          listen_address="http://host.docker.internal:9000"
       fi
 else
-    listen_address= https://codegen-staging.substreams.dev
+    listen_address="https://codegen-staging.substreams.dev"
 fi
 
 substreams init --state-file /app/generator.json --force-download-cwd --codegen-endpoint $listen_address
