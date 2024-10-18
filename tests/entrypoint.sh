241b67bc3281671a1c2e3def9fb6a455c1206c11
arnaudberger <arnaudberger84@gmail.com>
2024-10-18 12:53:05 -0400

    test linux hostname

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -2,7 +2,7 @@
 set -euxo pipefail
 
 if [ "$TEST_LOCAL_CODEGEN" = "true" ]; then
-    substreams init --state-file /app/generator.json --force-download-cwd --codegen-endpoint http://host.docker.internal:9000
+    substreams init --state-file /app/generator.json --force-download-cwd --codegen-endpoint http://172.17.0.1:9000
 else
     substreams init --state-file /app/generator.json --force-download-cwd --codegen-endpoint https://codegen-staging.substreams.dev
 fi
