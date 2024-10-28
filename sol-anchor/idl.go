05d9587a006ce8b78f5f125f49dcd4687216f0e4
Enol Álvarez <enol826@gmail.com>
2024-10-28 13:07:35 +0100

    Add support for vec and option

--- sol-anchor/idl.go
+++ sol-anchor/idl.go
@@ -61,12 +61,47 @@ func (f *Field) SnakeCaseNameWithoutInitialUnderscore() string {
 	return toSnakeCase(f.Name, false)
 }
 
+// FIELD TYPE
+
+type FieldTypeArray struct {
+	Type   string
+	Length string
+}
+
+type FieldTypeVec struct {
+	Type      string
+	IsDefined bool
+}
+
+type FieldTypeOption struct {
+	Simple  *string
+	Defined *string
+	Vec     *FieldTypeVec
+	Array   *FieldTypeArray
+}
+
+func (f *FieldTypeOption) IsSimple() bool {
+	return f.Simple != nil
+}
+
+func (f *FieldTypeOption) IsDefined() bool {
+	return f.Defined != nil
+}
+
+func (f *FieldTypeOption) IsVec() bool {
+	return f.Vec != nil
+}
+
+func (f *FieldTypeOption) IsArray() bool {
+	return f.Array != nil
+}
+
 type FieldType struct {
-	Simple     string
-	Defined    string
-	Array      string
-	VecSimple  string
-	VecDefined string
+	Simple  string
+	Defined string
+	Array   *FieldTypeArray
+	Vec     *FieldTypeVec
+	Option  *FieldTypeOption
 }
 
 func (t *FieldType) IsSimple() bool {
@@ -82,15 +117,15 @@ func (t *FieldType) IsDefined() bool {
 }
 
 func (t *FieldType) IsArray() bool {
-	return t.Array != ""
+	return t.Array != nil
 }
 
-func (t *FieldType) IsVecDefined() bool {
-	return t.VecDefined != ""
+func (t *FieldType) IsVec() bool {
+	return t.Vec != nil
 }
 
-func (t *FieldType) IsVecSimple() bool {
-	return t.VecSimple != ""
+func (t *FieldType) IsOption() bool {
+	return t.Option != nil
 }
 
 func (t *FieldType) Resolve() string {
@@ -107,7 +142,29 @@ func (t *FieldType) Resolve() string {
 	}
 
 	if t.IsArray() {
-		return t.Array
+		return t.Array.Type
+	}
+
+	if t.IsVec() {
+		return t.Vec.Type
+	}
+
+	if t.IsOption() {
+		if t.Option.IsSimple() {
+			return *t.Option.Simple
+		}
+
+		if t.Option.IsDefined() {
+			return *t.Option.Defined
+		}
+
+		if t.Option.IsVec() {
+			return t.Option.Vec.Type
+		}
+
+		if t.Option.IsArray() {
+			return t.Array.Type
+		}
 	}
 
 	return ""
@@ -115,6 +172,68 @@ func (t *FieldType) Resolve() string {
 
 func (t *FieldType) ResolveProtobufType() string {
 	return ToProtobufType(t.Resolve())
+}
+
+func PrintSimple(simpleType string, fieldName string, variableName string, isOptional bool) string {
+	fieldNameSnakeCase := toSnakeCase(fieldName, true)
+	fieldNameSnakeCaseWithoutInitialUnderscore := toSnakeCase(fieldName, false)
+
+	// If cast is needed
+	cast := CastInRustIfNeeded(simpleType)
+	if cast != "" {
+		if isOptional {
+			return fmt.Sprintf("%s: Some(%s.%s as %s),", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase, cast)
+		}
+		return fmt.Sprintf("%s: %s.%s as %s,", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase, cast)
+	}
+
+	// If cast is NOT needed
+	if isOptional {
+		return fmt.Sprintf("%s: Some(%s.%s),", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase)
+	}
+	return fmt.Sprintf("%s: %s.%s,", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase)
+}
+
+func PrintDefined(typeName string, fieldName string, variableName string, types []Type, isVec bool, isOption bool) string {
+	for _, t := range types {
+		if t.Name == typeName {
+			if t.Type.IsEnum() {
+				return fmt.Sprintf("%s: map_enum_%s(%s.%s),", toSnakeCase(fieldName, false), t.SnakeCaseName(), toSnakeCase(variableName, true), toSnakeCase(fieldName, true))
+			} else {
+				var fieldsInString strings.Builder
+				for _, structField := range t.Type.Struct.Fields {
+					fieldString := fmt.Sprintf("%s.%s", toSnakeCase(variableName, true), toSnakeCase(fieldName, true))
+					if variableName == "" {
+						fieldString = toSnakeCase(fieldName, true)
+					}
+
+					if isOption {
+						fieldString = fmt.Sprintf("%s: map_option_%s(%s.%s),", toSnakeCase(fieldName, true), toSnakeCase(fieldName, true), toSnakeCase(variableName, true), toSnakeCase(fieldName, true))
+						fieldsInString.WriteString(fieldString)
+						continue
+					}
+
+					fieldsInString.WriteString(structField.Type.Print(structField.Name, fieldString, types))
+				}
+
+				if isVec {
+					return fmt.Sprintf(`%s {
+						%s
+					}`, t.Name, fieldsInString.String())
+				}
+
+				if isOption {
+					return fieldsInString.String()
+				}
+
+				return fmt.Sprintf(`%s: Some(%s {
+						%s
+					}),`, toSnakeCase(fieldName, false), t.Name, fieldsInString.String())
+			}
+		}
+	}
+
+	return ""
 }
 
 func (f *FieldType) Print(fieldName string, variableName string, types []Type) string {
@@ -126,29 +245,37 @@ func (f *FieldType) Print(fieldName string, variableName string, types []Type) s
 	}
 
 	if f.IsSimple() {
-		cast := CastInRustIfNeeded(f.Simple)
-		if cast != "" {
-			return fmt.Sprintf("%s: %s.%s as %s,", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldName, cast)
-		}
-
-		return fmt.Sprintf("%s: %s.%s,", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase)
+		return PrintSimple(f.Simple, fieldName, variableName, false)
 	}
 
 	if f.IsDefined() {
-		for _, t := range types {
-			if t.Name == f.Defined {
-				if t.Type.IsEnum() {
-					return fmt.Sprintf("%s: map_enum_%s(%s.%s),", toSnakeCase(fieldName, false), t.SnakeCaseName(), variableName, toSnakeCase(fieldName, true))
-				} else {
-					var fieldsInString strings.Builder
-					for _, structField := range t.Type.Struct.Fields {
-						fieldsInString.WriteString(structField.Type.Print(structField.Name, variableName, types))
-					}
-					return fmt.Sprintf(`%s: %s {
-							%s
-						},`, toSnakeCase(fieldName, false), t.Name, fieldsInString.String())
-				}
-			}
+		return PrintDefined(f.Defined, fieldName, variableName, types, false, false)
+	}
+
+	if f.IsArray() {
+		cast := CastInRustIfNeeded(f.Array.Type)
+		if cast == "" {
+			return fmt.Sprintf("%s: %s.%s.to_vec(),", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase)
+		}
+
+		return fmt.Sprintf("%s: %s.%s.into_iter().map(|f| f as %s).collect(),", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase, cast)
+	}
+
+	if f.IsVec() && f.Vec.IsDefined {
+		return fmt.Sprintf("%s: %s.%s.into_iter().map(|%s| %s).collect(),", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase, fieldNameSnakeCase, PrintDefined(f.Vec.Type, fieldNameSnakeCase, "", types, true, false))
+	}
+
+	if f.IsVec() && !f.Vec.IsDefined {
+		return fmt.Sprintf("%s: %s.%s,", fieldNameSnakeCaseWithoutInitialUnderscore, variableName, fieldNameSnakeCase)
+	}
+
+	if f.IsOption() {
+		if f.Option.IsSimple() {
+			return PrintSimple(*f.Option.Simple, fieldName, variableName, true)
+		}
+
+		if f.Option.IsDefined() {
+			return PrintDefined(*f.Option.Defined, fieldName, variableName, types, false, true)
 		}
 	}
 
@@ -161,8 +288,8 @@ A field could be of type:
     - defined
     - array
     - vec
+	- optional (simple, defined or vec)
 */
-
 func unmarshalDefined(data []byte) string {
 	var definedType struct {
 		Defined string `json:"defined"`
@@ -185,7 +312,6 @@ func unmarshalSimple(data []byte) string {
 
 /*
 Return types:
-
 	bool: isDefined (true/false)
 	string: type (simple/defined)
 */
@@ -215,6 +341,58 @@ func unmarshalVec(data []byte) (bool, string) {
 	return false, ""
 }
 
+/*
+Return types:
+	- option type (simple, defined or vec)
+	- type
+*/
+func unmarshalOption(data []byte) (string, string) {
+	// Try simple
+	var optionSimpleType struct {
+		Option string `json:"option"`
+	}
+	if err := json.Unmarshal(data, &optionSimpleType); err == nil && optionSimpleType.Option != "" {
+		return "simple", optionSimpleType.Option
+	}
+
+	// Try defined
+	type DefinedType struct {
+		Defined string `json:"defined"`
+	}
+	var optionDefinedType struct {
+		Option DefinedType `json:"option"`
+	}
+	if err := json.Unmarshal(data, &optionDefinedType); err == nil && optionDefinedType.Option.Defined != "" {
+		return "defined", optionDefinedType.Option.Defined
+	}
+
+	// Try vec simple
+	type VecTypeSimple struct {
+		Vec string `json:"vec"`
+	}
+	var optionVecSimpleType struct {
+		Option VecTypeSimple `json:"option"`
+	}
+	if err := json.Unmarshal(data, &optionVecSimpleType); err == nil && optionVecSimpleType.Option.Vec != "" {
+		return "vecSimple", optionVecSimpleType.Option.Vec
+	}
+
+	// Try vec defined
+	type VecTypeDefined struct {
+		Vec DefinedType `json:"vec"`
+	}
+	var optionVecDefinedType struct {
+		Option VecTypeDefined `json:"option"`
+	}
+	if err := json.Unmarshal(data, &optionVecDefinedType); err == nil && optionVecDefinedType.Option.Vec.Defined != "" {
+		return "vecDefined", optionVecDefinedType.Option.Vec.Defined
+	}
+
+	// TODO: Try array?
+
+	return "", ""
+}
+
 func (t *FieldType) UnmarshalJSON(data []byte) error {
 	if result := unmarshalSimple(data); result != "" {
 		t.Simple = result
@@ -230,20 +408,45 @@ func (t *FieldType) UnmarshalJSON(data []byte) error {
 		Array []interface{} `json:"array"`
 	}
 	if err := json.Unmarshal(data, &arrayType); err == nil && len(arrayType.Array) > 0 {
-		stringType, ok1 := arrayType.Array[0].(string)
-		if ok1 {
-			t.Array = stringType
+		stringType, stringOk := arrayType.Array[0].(string)
+		// TODO: The length should be in position [1] of the array, but it's not decoded correctly. Why?
+		//intType, intOk := arrayType.Array[1].(int)
+
+		if stringOk {
+			t.Array = &FieldTypeArray{
+				Type:   stringType,
+				Length: string(0),
+			}
 		}
 		return nil
 	}
 
 	isDefined, result := unmarshalVec(data)
 	if result != "" {
-		if isDefined {
-			t.VecDefined = result
-		} else {
-			t.VecSimple = result
+		t.Vec = &FieldTypeVec{
+			Type:      result,
+			IsDefined: isDefined,
 		}
+
+		return nil
+	}
+
+	fieldType, result := unmarshalOption(data)
+	if fieldType != "" && result != "" {
+		var fieldTypeOption FieldTypeOption
+
+		switch fieldType {
+		case "simple":
+			fieldTypeOption = FieldTypeOption{Simple: &result}
+		case "defined":
+			fieldTypeOption = FieldTypeOption{Defined: &result}
+		case "vecSimple":
+			fieldTypeOption = FieldTypeOption{Vec: &FieldTypeVec{Type: result, IsDefined: false}}
+		case "vecDefined":
+			fieldTypeOption = FieldTypeOption{Vec: &FieldTypeVec{Type: result, IsDefined: true}}
+		}
+
+		t.Option = &fieldTypeOption
 
 		return nil
 	}
@@ -260,6 +463,10 @@ type Type struct {
 
 func (t *Type) SnakeCaseName() string {
 	return toSnakeCase(t.Name, true)
+}
+
+func (t *Type) PascalCaseName() string {
+	return textcase.PascalCase(t.Name)
 }
 
 type TypeDetails struct {
@@ -340,6 +547,12 @@ func ToProtobufType(rustType string) string {
 	switch rustType {
 	case "u8":
 		return "uint64"
+	case "u16":
+		return "uint64"
+	case "u128":
+		return "uint64"
+	case "i128":
+		return "uint64"
 	case "u64":
 		return "uint64"
 	case "i64":
@@ -363,30 +576,12 @@ func CastInRustIfNeeded(rustType string) string {
 	switch rustType {
 	case "u8":
 		return "u64"
-	}
-
-	return ""
-}
-
-func PrintFieldTypeRecursively(fieldName string, fieldType FieldType, types []Type) string {
-	if fieldType.IsSimple() {
-		return fmt.Sprintf("%s: %s", toSnakeCase(fieldName, false), fieldName)
-	}
-
-	if fieldType.IsDefined() {
-		for _, t := range types {
-			if t.Type.IsEnum() {
-
-			} else {
-				var fieldsInString strings.Builder
-				for _, f := range t.Type.Struct.Fields {
-					fieldsInString.WriteString(PrintFieldTypeRecursively(f.SnakeCaseName(), f.Type, types))
-				}
-				return fmt.Sprintf(`%s {
-					%s
-				}`, t.Name, fieldsInString.String())
-			}
-		}
+	case "u16":
+		return "u64"
+	case "u128":
+		return "u64"
+	case "i128":
+		return "u64"
 	}
 
 	return ""
