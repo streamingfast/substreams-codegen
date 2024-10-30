0ec39822397a848c862730def002f6177f7d6462
arnaudberger <arnaudberger84@gmail.com>
2024-10-30 15:54:47 -0400

    change or to &&

--- evm-events-calls/state.go
+++ evm-events-calls/state.go
@@ -343,7 +343,7 @@ func (d DynamicContract) FactoryInitialBlock() uint64 {
 	return *d.parentContract.InitialBlock
 }
 func (d DynamicContract) GenerateStoreQuery() string {
-	return fmt.Sprintf("evt_addr:%s || evt_sig:%s", d.parentContract.Address, "0x"+d.parentContract.FactoryCreationEvent)
+	return fmt.Sprintf("evt_addr:%s && evt_sig:%s", d.parentContract.Address, "0x"+d.parentContract.FactoryCreationEvent)
 }
 func (d DynamicContract) ParentContract() *Contract   { return d.parentContract }
 func (d DynamicContract) Identifier() string          { return d.Name }
