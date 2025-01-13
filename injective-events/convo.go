35b564c40e1bc9f4b1913c4eaab1b6964e4a79ee
Enol <51820585+enoldev@users.noreply.github.com>
2025-01-13 16:13:13 +0100

    Simplify injective-events codegen (#18)

--- injective-events/convo.go
+++ injective-events/convo.go
@@ -42,6 +42,7 @@ func (c *Convo) contextEventDesc() *eventDesc {
 
 func (c *Convo) NextStep() (out loop.Cmd) {
 	p := c.State
+	p.DataType = "events"
 
 	if p.Name == "" {
 		return cmd(codegen.AskProjectName{})
