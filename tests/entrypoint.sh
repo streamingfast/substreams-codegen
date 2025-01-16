e518994efc8d4ddb44d9a5eaa03004a84b3719ed
Enol Álvarez <enol826@gmail.com>
2025-01-16 13:09:00 +0100

    WIP

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -3,6 +3,24 @@ set -euxo pipefail
 
 substreams init --state-file /app/generator.json --force-download-cwd
 
+cat > myfile.yaml <<EOF
+
+version: v1
+plugins:
+- name: neoeinstein-prost
+  out: ./src/pb
+  opt:
+    - file_descriptor_set=false
+  path: /app/protoc-gen-prost-protoc-gen-tonic-v0.4.1/target/release/protoc-gen-prost
+  
+
+- plugin: neoeinstein-prost-crate
+  out: ./src/pb
+  opt:
+    - no_features
+  path: /app/protoc-gen-prost-protoc-gen-tonic-v0.4.1/target/release/protoc-gen-crate
+EOF
+
 substreams build
 
 ## To validate the manifest
