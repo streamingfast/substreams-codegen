ffb775732d2565ba3c4da981e63c711194f813a5
Enol Álvarez <enol826@gmail.com>
2025-01-16 14:14:46 +0100

    WIP1

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -11,14 +11,14 @@ plugins:
   out: ./src/pb
   opt:
     - file_descriptor_set=false
-  path: /app/protoc-gen-prost-protoc-gen-tonic-v0.4.1/target/release/protoc-gen-prost
+  path: /app/protoc-gen-prost-protoc-gen-prost-v0.3.1/target/release/protoc-gen-prost
   
 
 - plugin: neoeinstein-prost-crate
   out: ./src/pb
   opt:
     - no_features
-  path: /app/protoc-gen-prost-protoc-gen-tonic-v0.4.1/target/release/protoc-gen-prost-crate
+  path: /app/protoc-gen-prost-protoc-gen-prost-v0.3.1/target/release/protoc-gen-prost-crate
 EOF
 
 substreams build
