52d4d67bb86f3a8981f84b327fba2ce5a46ceff8
Enol Álvarez <enol826@gmail.com>
2025-01-16 14:36:09 +0100

    Fix out path

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -8,14 +8,14 @@ cat > buf.gen.yaml <<EOF
 version: v1
 plugins:
 - name: neoeinstein-prost
-  out: ./src/pb
+  out: src/pb
   opt:
     - file_descriptor_set=false
   path: /app/protoc-gen-prost-protoc-gen-prost-v0.3.1/target/release/protoc-gen-prost
   
 
 - plugin: neoeinstein-prost-crate
-  out: ./src/pb
+  out: src/pb
   opt:
     - no_features
   path: /app/protoc-gen-prost-protoc-gen-prost-v0.3.1/target/release/protoc-gen-prost-crate
