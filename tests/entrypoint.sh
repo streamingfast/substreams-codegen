4750a9ac312b8b64f37887bda438a33a34d8d6ae
Enol Álvarez <enol826@gmail.com>
2025-01-17 13:53:06 +0100

    wip

--- tests/entrypoint.sh
+++ tests/entrypoint.sh
@@ -7,14 +7,14 @@ cat > buf.gen.yaml <<EOF
 version: v1
 plugins:
 - name: neoeinstein-prost
-  out: src/pb
+  out: ./src/pb
   opt:
     - file_descriptor_set=false
   path: /app/protoc-gen-prost-protoc-gen-prost-v0.3.1/target/release/protoc-gen-prost
   
 
 - plugin: neoeinstein-prost-crate
-  out: src/pb
+  out: ./src/pb
   opt:
     - no_features
   path: /app/protoc-gen-prost-protoc-gen-prost-v0.3.1/target/release/protoc-gen-prost-crate
@@ -22,14 +22,7 @@ EOF
 
 ls
 cat buf.gen.yaml
-#ls src/pb
 
-
-#rm -r src/pb
-#ls src
-#ls src/pb
-substreams protogen
-ls src/pb
 substreams build
 
 ## To validate the manifest
