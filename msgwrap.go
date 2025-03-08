b446ffe661c45751763f1b720ee809d9503d99d0
Enol <51820585+enoldev@users.noreply.github.com>
2025-03-08 16:21:19 +0100

    Revert "Add read from local file (#23)" (#24)
    This reverts commit 0bcf7d1c99ff79496dc476f802d827d9ff02a70d.

--- msgwrap.go
+++ msgwrap.go
@@ -201,16 +201,6 @@ func (w *MsgWrap) TextInput(prompt string, submitButtonLabel string) *MsgWrap {
 	return w
 }
 
-func (w *MsgWrap) LocalFile(prompt string, submitButtonLabel string) *MsgWrap {
-	w.Msg.Entry = &pbconvo.SystemOutput_LocalFile_{
-		LocalFile: &pbconvo.SystemOutput_LocalFile{
-			Prompt:            prompt,
-			SubmitButtonLabel: submitButtonLabel,
-		},
-	}
-	return w
-}
-
 func (w *MsgWrap) DefaultValue(value string) *MsgWrap {
 	switch entry := w.Msg.Entry.(type) {
 	case *pbconvo.SystemOutput_TextInput_:
