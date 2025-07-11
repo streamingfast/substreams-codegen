33b7c0b6b83ecac5cb045f9629b31947654f3024
Enol Álvarez <enol826@gmail.com>
2025-07-11 11:05:34 +0200

    fix

--- evm-hello-world/chain_configs.go
+++ evm-hello-world/chain_configs.go
@@ -14,6 +14,7 @@ type ChainConfig struct {
 	Network              string
 	SupportsCalls        bool
 	APIKeyEnvVar         string
+	ContractAddress      string
 
 	initialBlockCache map[string]uint64
 }
@@ -27,6 +28,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "mainnet",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",
 	},
 	"bnb": {
 		DisplayName:          "BNB",
@@ -34,6 +36,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "bsc",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x8965349fb649a33a30cbfda057d8ec2c48abe2a2",
 	},
 	"polygon": {
 		DisplayName:          "Polygon",
@@ -41,6 +44,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "polygon",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
 	},
 	"amoy": {
 		DisplayName:          "Polygon Amoy Testnet",
@@ -48,6 +52,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "amoy",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x41E94Eb019C0762f9Bfcf9Fb1E58725BfB0e7582",
 	},
 	"arbitrum": {
 		DisplayName:          "Arbitrum",
@@ -55,6 +60,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		FirstStreamableBlock: 0,
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0xaf88d065e77c8cC2239327C5EDb3A432268e5831",
 	},
 	"holesky": {
 		DisplayName:          "Holesky",
@@ -62,6 +68,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "holesky",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x74a4a85c611679b73f402b36c0f84a7d2ccdfda3",
 	},
 	"sepolia": {
 		DisplayName:          "Sepolia Testnet",
@@ -69,6 +76,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "sepolia",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238",
 	},
 	"optimism": {
 		DisplayName:          "Optimism Mainnet",
@@ -77,6 +85,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        false,
 		APIKeyEnvVar:         "CODEGEN_OPTIMISM_API_KEY",
+		ContractAddress:      "0x0b2c639c533813f4aa9d7837caf62653d097ff85",
 	},
 	"avalanche-mainnet": {
 		DisplayName:          "Avalanche C-chain",
@@ -84,6 +93,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "avalanche-mainnet",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        false,
+		ContractAddress:      "0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E",
 	},
 	"chapel": {
 		DisplayName:          "BNB Chapel Testnet",
@@ -91,6 +101,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "chapel",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x337610d27c682e347c9cd60bd4b3b107c9d34ddd",
 	},
 	"sei-mainnet": {
 		DisplayName:          "SEI Mainnet (EVM)",
@@ -98,6 +109,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "sei-mainnet",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x3894085ef7ff0f0aedf52e2a2704928d1ec074f1",
 	},
 	"base-mainnet": {
 		DisplayName:          "Base Mainnet",
@@ -105,6 +117,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "base-mainnet",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913",
 	},
 	"tron-evm-mainnet": {
 		DisplayName:          "Tron EVM mainnet",
@@ -112,6 +125,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "tron-evm-mainnet",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        false,
+		ContractAddress:      "TR7NHqjeKQxGTCi8q8ZY4pL8otSzgjLj6t",
 	},
 	"unichain": {
 		DisplayName:          "Unichain Mainnet",
@@ -119,6 +133,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "unichain",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "0x078D782b760474a361dDA0AF3839290b0EF57AD6",
 	},
 	"injective-evm-testnet": {
 		DisplayName:          "Injective EVM testnet",
@@ -126,6 +141,7 @@ var ChainConfigByID = map[string]*ChainConfig{
 		Network:              "injective-evm-testnet",
 		initialBlockCache:    make(map[string]uint64),
 		SupportsCalls:        true,
+		ContractAddress:      "inj1d4y0dcwh2wrgv9rcke0hx6rs2wp4w4vxds87ep",
 	},
 }
 
